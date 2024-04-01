48a8651e7836364b4db3da0bd603ed511808bbef
Greg Field <greg.field1992@gmail.com>
2024-04-01 11:35:47 +0100

    add modules to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    'vuetify-nuxt-module',
+    '@pinia/nuxt',
+    '@nuxt/devtools',
+    '@vueuse/nuxt',
+    'nuxt-mongoose',
+    '@nuxtjs/eslint-module',
+    'nuxt-bugsnag',
+    '@sidebase/nuxt-auth',
+    '@nuxtjs/color-mode'
+  ],
   devtools: { enabled: false }
 })
